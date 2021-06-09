50ca388ffc4085d84d111df899510516571d8c79
Kasun Niroshana <zapniroshana420@gmail.com>
2021-06-09 21:28:17 +0530

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Kasun Niroshana on 6/9/21.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
